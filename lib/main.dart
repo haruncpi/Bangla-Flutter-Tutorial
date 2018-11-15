498967e57d63556b1fc71944211061acd0935549
MD.HARUN-UR-RASHID <harun.cox@gmail.com>
2018-11-15 23:34:47 +0600

    EP-04

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,26 @@ import 'package:flutter/material.dart';
 // stateless, statefull
 
 void main(){
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: 'My App',
+    home: HomePage()
+  ));
 }
 
-class MyApp extends StatelessWidget{
+class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return  MaterialApp(
-      title: 'My App',
-      home: Scaffold(
+    return  Scaffold(
         appBar:  AppBar(title:  Text('Home')),
-        body: Center(
-          child: Text('This is our first flutter app'),
-        ) 
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Text('This is our first flutter app. This is our first flutter appThis is our first flutter appThis is our first flutter appThis is our first flutter appThis is our first flutter app',
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(color: Colors.green,fontSize: 15,fontStyle: FontStyle.italic)
+                  )
         
-      )
-    );
+        )
+         
+      );
   }
 }
