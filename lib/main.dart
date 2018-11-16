3c315b127abee31351dc1facbf42025227fa1326
MD.HARUN-UR-RASHID <harun.cox@gmail.com>
2018-11-17 00:02:47 +0600

    EP-06

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ void main() {
 }
 
 class HomePage extends StatelessWidget {
+  final _longText =
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse feugiat.. Etiam ultricies nisi vel augue. Sed a libero. Sed libero. Morbi vestibulum volutpat enim. Praesent ut ligula non mi varius sagittis. Vestibulum rutrum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante vel mi. Sed lectus.";
+  final _shortText = "Pellentesque habitant morbi tristique senectus et netus";
+
   @override
   Widget build(BuildContext context) {
     double _width = MediaQuery.of(context).size.width;
@@ -15,20 +19,53 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Home'),
         ),
-        body: Column(
+        body: ListView(
           children: <Widget>[
+            
+            Column(
+              children: <Widget>[
+                Image.asset("assets/images/image1.png"),
+                Container(
+                  child: Text(
+                    _longText,
+                    textAlign: TextAlign.justify,
+                  ),
+                  padding: EdgeInsets.all(20),
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(left: 20, top: 10),
+                      padding: EdgeInsets.all(5),
+                      height: 40,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Color(Helper.getHexToInt("#f1c40f")),
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Icon(Icons.linked_camera),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 20, top: 10),
+                      padding: EdgeInsets.all(5),
+                      height: 40,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Color(Helper.getHexToInt("#f1c40f")),
+                          borderRadius: BorderRadius.circular(8)),
+                      child: Icon(Icons.access_alarm),
+                    ),
+                  ],
+                )
+              ],
+            ),
             Container(
-              margin: EdgeInsets.only(left: 20, top: 10),
-              padding: EdgeInsets.all(20),
-              height: 40,
-              width: 50,
-              decoration: BoxDecoration(
-                  color: Color(Helper.getHexToInt("#f1c40f")),
-                  borderRadius: BorderRadius.circular(8)),
-                  
-            )
+                  child: Text(
+                    _longText,
+                    textAlign: TextAlign.justify,
+                  ),
+                  padding: EdgeInsets.all(20),
+                ),
           ],
         ));
   }
-
 }
