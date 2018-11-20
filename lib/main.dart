a3567b54751fbbf39b5a762b2ba3bf34bb7bf4ea
MD.HARUN-UR-RASHID <harun.cox@gmail.com>
2018-11-20 23:32:04 +0600

    EP-07

--- lib/main.dart
+++ lib/main.dart
@@ -11,61 +11,54 @@ class HomePage extends StatelessWidget {
   final _longText =
       "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse feugiat.. Etiam ultricies nisi vel augue. Sed a libero. Sed libero. Morbi vestibulum volutpat enim. Praesent ut ligula non mi varius sagittis. Vestibulum rutrum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante vel mi. Sed lectus.";
   final _shortText = "Pellentesque habitant morbi tristique senectus et netus";
+  
 
   @override
   Widget build(BuildContext context) {
-    double _width = MediaQuery.of(context).size.width;
     return Scaffold(
         appBar: AppBar(
-          title: Text('Home'),
+          title: Text('Home')
         ),
-        body: ListView(
+
+        body: Column(
           children: <Widget>[
-            
-            Column(
+            Stack( 
               children: <Widget>[
                 Image.asset("assets/images/image1.png"),
-                Container(
-                  child: Text(
-                    _longText,
-                    textAlign: TextAlign.justify,
-                  ),
-                  padding: EdgeInsets.all(20),
-                ),
-                Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(left: 20, top: 10),
-                      padding: EdgeInsets.all(5),
-                      height: 40,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          color: Color(Helper.getHexToInt("#f1c40f")),
-                          borderRadius: BorderRadius.circular(8)),
-                      child: Icon(Icons.linked_camera),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(left: 20, top: 10),
-                      padding: EdgeInsets.all(5),
-                      height: 40,
-                      width: 50,
-                      decoration: BoxDecoration(
-                          color: Color(Helper.getHexToInt("#f1c40f")),
-                          borderRadius: BorderRadius.circular(8)),
-                      child: Icon(Icons.access_alarm),
-                    ),
-                  ],
+                Positioned(
+                  bottom: 40,
+                  left: 30,
+                  child: Text("Hello Flutter",style: TextStyle(color: Colors.black,fontSize: 20),),
                 )
               ],
             ),
-            Container(
-                  child: Text(
-                    _longText,
-                    textAlign: TextAlign.justify,
-                  ),
-                  padding: EdgeInsets.all(20),
-                ),
+            SizedBox(
+              height: 40,
+            ),
+            Text("Features",style: TextStyle(color: Colors.black,fontSize: 20)),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: <Widget>[
+                _rowCell("#3498db"),
+                _rowCell("#e74c3c"),
+                _rowCell("#2ecc71"),
+                _rowCell("#34495e")
+              ]
+            )
           ],
-        ));
+        )
+    );
+  }
+
+
+  Widget _rowCell(String clr){
+    return Expanded(
+      child: Container(
+          height: 50,width: 50,decoration: BoxDecoration(color: Color(Helper.getHexToInt(clr))),
+          child: Icon(Icons.link,color: Colors.white),
+        )
+    );
   }
 }
